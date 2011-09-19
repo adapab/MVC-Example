e8c8dab2ed6770c07351f60810b7bb49d1fd544e
adapab <adapab@zlab.com.au>
2011-09-19 22:05:48 +0530

    Backbone Example

---
+++ Simple/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Simple.Models;
+
+namespace Simple.Controllers
+{
+    public class HomeController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+         
+        
+        public ActionResult Index()
+        {
+            
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Simple/Controllers/TodoListController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Simple.Models;
+
+namespace Simple.Controllers
+{
+    public class TodoListController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+       
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+         #region Json Methods Used for Insert todoList Element
+        public JsonResult InsertTodoItem(String id)
+        {
+             
+            try
+            {
+                Simple.Models.Datum oDatum = new Simple.Models.Datum();
+                oDatum.Todo = id;
+                oDatum.Sort =db.Data.Count()+1;
+                db.Data.AddObject(oDatum);
+                db.SaveChanges();
+
+                return Json(true, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex) { }
+            return null;
+        }
+
+
+#endregion
+       
+
+    }
+}
